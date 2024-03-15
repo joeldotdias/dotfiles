df1074c903eb7671117a16cffd509d6f5245975c
joeldotdias <joeldias0204@gmail.com>
2024-03-15 18:07:17 +0530

    Better scripts

--- nvim/lua/lemons/plugins/color.lua
+++ nvim/lua/lemons/plugins/color.lua
@@ -1,8 +1,8 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine"
+    color = color or "rose-pine-moon"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none"})	
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 end
 
@@ -11,7 +11,7 @@ return {
     name = "rose-pine",
     lazy = false,
     priority = 1000,
-    
+
     config = function()
         require("rose-pine").setup({
             disable_background = true
